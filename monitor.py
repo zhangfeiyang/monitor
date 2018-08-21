6384b3ffd1a4669f5f53e445136e21b3396bc93f
zhangfeiyang <1173205582@qq.com>
2018-08-21 15:50:05 +0800

    modified monitot.py, the first version doesn't work

--- monitor.py
+++ monitor.py
@@ -5,13 +5,16 @@ import os
 import time
 import random 
 while True:
-	lines = os.popen('condor_q zhangfy').readlines()
+	lines = os.popen('hep_q -u zhangfy').readlines()
 	jobs = len(lines)-4
 
 	while jobs < 1005:
-		First = lines[-3].split("_")[1].split(' ')[0]
+		First = lines[-3].split("_")[1].split('.')[0]
 		First = int(First)+1 
 		cmd = "./cls.py "+str(First)+" "+str(First+100)
 		os.system(cmd)
+
+		lines = os.popen('hep_q -u zhangfy').readlines()
+		jobs = len(lines)-4
 
 	time.sleep(5)
