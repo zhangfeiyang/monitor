969c3b9ffb0b91bed7aa31d456a68b1a32086bf5
zhangfeiyang <1173205582@qq.com>
2018-08-27 09:04:15 +0800

    update monitor.py

--- monitor.py
+++ monitor.py
@@ -6,15 +6,22 @@ import time
 import random 
 while True:
 	lines = os.popen('hep_q -u zhangfy').readlines()
+	while 'ERROR' in lines:
+		lines = os.popen('hep_q -u zhangfy').readlines()
 	jobs = len(lines)-4
 
 	while jobs < 1005:
 		First = lines[-3].split("_")[1].split('.')[0]
 		First = int(First)+1 
-		cmd = "./cls.py "+str(First)+" "+str(First+100)
+		seed = int(random.uniform(0,2))
+		if seed == 0:
+			cmd = "./cls4.py "+str(First)+" "+str(First+10)
+		else:
+			cmd = "./cls5.py "+str(First)+" "+str(First+10)
+			
 		os.system(cmd)
 
 		lines = os.popen('hep_q -u zhangfy').readlines()
 		jobs = len(lines)-4
 
-	time.sleep(5)
+	time.sleep(30)
